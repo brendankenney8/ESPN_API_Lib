01de13a59d59dc316da8e7d0f5eb8535f82b1b3a
Brendan Kenney <brendankenney3@gmail.com>
2025-08-31 14:11:30 -0400

    doesn't work 100% yet, but setting up json to objects

--- src/espn_api/nfl/Game.py
+++ src/espn_api/nfl/Game.py
@@ -1,31 +1,105 @@
-from espn_api.utilities.async_utils import run as _run
+from datetime import datetime
 from espn_api.utilities.request import get as get_request
 from espn_api.nfl.NFLData import NFLData
+from espn_api.nfl.Odds import Odds
 
 
 class Game(NFLData):
-    def __init__(self, teams, score, odds, stats):
-        self.teams = teams
-        self.score = score
-        self.odds  = odds
-        self.stats = stats
+    def __init__(self, game_data, odds_data=None):
+        """
+        Initialize a Game object with game data and optional odds data
+        """
+        self.game_id = game_data.get('id')
+        self.status = game_data.get('status', {}).get('type', {}).get('name')
+        self.date = self._parse_datetime(game_data.get('date'))
+        self.venue = game_data.get('competitions', [{}])[0].get('venue', {}).get('fullName')
+        self.location = game_data.get('competitions', [{}])[0].get('location')
+        self.attendance = game_data.get('competitions', [{}])[0].get('attendance')
+        
+        # Team information
+        self.competitors = self._parse_competitors(game_data.get('competitions', [{}])[0].get('competitors', []))
+        self.home_team = next((team for team in self.competitors if team.get('homeAway') == 'home'), {})
+        self.away_team = next((team for team in self.competitors if team.get('homeAway') == 'away'), {})
+        
+        # Odds
+        self.odds = Odds(odds_data) if odds_data else None
+        
+        # Game details
+        self.details = {
+            'season': game_data.get('season', {}).get('year'),
+            'week': game_data.get('week', {}).get('number'),
+            'season_type': game_data.get('season', {}).get('type'),
+            'neutral_site': game_data.get('competitions', [{}])[0].get('neutralSite', False),
+            'conference_competition': game_data.get('competitions', [{}])[0].get('conferenceCompetition', False),
+            'play_by_play_available': game_data.get('playByPlaySource'),
+            'broadcasts': [broadcast.get('names', []) for broadcast in game_data.get('competitions', [{}])[0].get('broadcasts', [])]
+        }
 
-
-    @staticmethod
-    async def _get_async(gameID):
-        if not isinstance(gameID, int):
-            raise TypeError("GameID must be an int")
-
+    def _parse_competitors(self, competitors_data):
+        """Parse the competitors data into a more usable format"""
+        parsed = []
+        for competitor in competitors_data:
+            team = competitor.get('team', {})
+            parsed.append({
+                'id': team.get('id'),
+                'name': team.get('displayName'),
+                'abbreviation': team.get('abbreviation'),
+                'score': competitor.get('score'),
+                'homeAway': competitor.get('homeAway'),
+                'winner': competitor.get('winner'),
+                'record': {
+                    'wins': competitor.get('record', [{}])[0].get('items', [{}])[0].get('stats', [{}])[0].get('value'),
+                    'losses': competitor.get('record', [{}])[0].get('items', [{}])[1].get('stats', [{}])[0].get('value'),
+                    'ties': competitor.get('record', [{}])[0].get('items', [{}])[2].get('stats', [{}])[0].get('value')
+                },
+                'logo': team.get('logo')
+            })
+        return parsed
+    
+    def _parse_datetime(self, date_str):
+        """Parse the datetime string from ESPN format"""
+        if not date_str:
+            return None
         try:
-            urls = [
-                f"http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/events/{gameID}",
-                f"http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/events/{gameID}/competitions/{gameID}/odds"
-            ]
-
-            all_data = await get_request(urls)
-
-            # format all_data here into a Game object
-
-            return all_data
+            return datetime.strptime(date_str, '%Y-%m-%dT%H:%MZ')
         except:
-            raise
+            return date_str
+
+    @classmethod
+    async def _get_async(cls, game_id):
+        """
+        Get game data by game ID
+        
+        Args:
+            game_id (int): The ESPN game ID
+            
+        Returns:
+            Game: A Game object with the game data
+        """
+        if not isinstance(game_id, int):
+            raise TypeError("game_id must be an integer")
+            
+        try:
+            # Define all the API endpoints we want to query
+            base_url = f"http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/events/{game_id}"
+            urls = [
+                base_url,  # Main game data
+                f"{base_url}/competitions/{game_id}/odds",  # Odds data
+                f"{base_url}/competitions/{game_id}/boxscore",  # Boxscore data
+                f"{base_url}/competitions/{game_id}/roster"  # Roster data
+            ]
+            
+            # Make all requests in parallel
+            all_data = await get_request(urls)
+            game_data = all_data[0]
+            odds_data = all_data[1]
+            # TODO: add others here later
+            
+            # Create and return Game object with all the data
+            return cls(game_data, odds_data)
+            
+        except Exception as e:
+            raise Exception(f"Error fetching game data: {str(e)}")
+    
+    def __str__(self):
+        return f"{self.away_team.get('name')} @ {self.home_team.get('name')} - {self.date.strftime('%Y-%m-%d %H:%M') if isinstance(self.date, datetime) else self.date}"
--- src/espn_api/nfl/Odds.py
+++ src/espn_api/nfl/Odds.py
@@ -1,11 +1,196 @@
-from espn_api.utilities.async_utils import run as _run
 from espn_api.utilities.request import get as get_request
 from espn_api.nfl.NFLData import NFLData
 
 class Odds(NFLData):
-    def __init__(self):
-        self.test = 0
+    def __init__(self, odds_data):
+        """
+        Initialize an Odds object with odds data
+        
+        Args:
+            odds_data (dict): Raw odds data from ESPN API
+        """
+        self.raw_data = odds_data
+        self.provider = self._get_provider(odds_data)
+        self.details = self._parse_odds_details(odds_data)
+        self.spread = self._parse_spread(odds_data)
+        self.moneyline = self._parse_moneyline(odds_data)
+        self.over_under = self._parse_over_under(odds_data)
+        self.last_updated = self._parse_last_updated(odds_data)
     
-    @staticmethod
-    async def _get_async():
-        return
+    def _get_provider(self, odds_data):
+        """Extract the odds provider information"""
+        if not odds_data or not isinstance(odds_data, dict):
+            return None
+            
+        provider = odds_data.get('provider', {})
+        return {
+            'id': provider.get('id'),
+            'name': provider.get('name'),
+            'priority': provider.get('priority')
+        }
+    
+    def _parse_odds_details(self, odds_data):
+        """Parse the main odds details"""
+        if not odds_data or not isinstance(odds_data, dict):
+            return {}
+            
+        return {
+            'details': odds_data.get('details'),
+            'over_under': odds_data.get('overUnder'),
+            'over_odds': odds_data.get('overOdds'),
+            'under_odds': odds_data.get('underOdds'),
+            'spread_odds': odds_data.get('spreadOdds'),
+            'away_team_odds': odds_data.get('awayTeamOdds', {}).get('favorite'),
+            'home_team_odds': odds_data.get('homeTeamOdds', {}).get('favorite'),
+            'money_line_away': odds_data.get('moneyLineAway'),
+            'money_line_home': odds_data.get('moneyLineHome'),
+            'money_line_draw': odds_data.get('moneyLineDraw'),
+            'open_date': odds_data.get('openDate'),
+            'last_updated': odds_data.get('lastUpdated'),
+            'point_spread_away': odds_data.get('pointSpreadAway'),
+            'point_spread_home': odds_data.get('pointSpreadHome'),
+            'point_spread_away_line': odds_data.get('pointSpreadAwayLine'),
+            'point_spread_home_line': odds_data.get('pointSpreadHomeLine'),
+            'score_away': odds_data.get('scoreAway'),
+            'score_home': odds_data.get('scoreHome'),
+            'vig_away': odds_data.get('vigAway'),
+            'vig_home': odds_data.get('vigHome')
+        }
+    
+    def _parse_spread(self, odds_data):
+        """Parse spread data"""
+        if not odds_data or not isinstance(odds_data, dict):
+            return {}
+            
+        return {
+            'away': {
+                'point_spread': odds_data.get('pointSpreadAway'),
+                'point_spread_line': odds_data.get('pointSpreadAwayLine'),
+                'vig': odds_data.get('vigAway'),
+                'odds': odds_data.get('spreadOdds')
+            },
+            'home': {
+                'point_spread': odds_data.get('pointSpreadHome'),
+                'point_spread_line': odds_data.get('pointSpreadHomeLine'),
+                'vig': odds_data.get('vigHome'),
+                'odds': -odds_data.get('spreadOdds') if odds_data.get('spreadOdds') else None
+            }
+        }
+    
+    def _parse_moneyline(self, odds_data):
+        """Parse moneyline data"""
+        if not odds_data or not isinstance(odds_data, dict):
+            return {}
+            
+        return {
+            'away': {
+                'odds': odds_data.get('moneyLineAway'),
+                'favorite': odds_data.get('awayTeamOdds', {}).get('favorite')
+            },
+            'home': {
+                'odds': odds_data.get('moneyLineHome'),
+                'favorite': odds_data.get('homeTeamOdds', {}).get('favorite')
+            },
+            'draw': {
+                'odds': odds_data.get('moneyLineDraw')
+            }
+        }
+    
+    def _parse_over_under(self, odds_data):
+        """Parse over/under data"""
+        if not odds_data or not isinstance(odds_data, dict):
+            return {}
+            
+        return {
+            'line': odds_data.get('overUnder'),
+            'over_odds': odds_data.get('overOdds'),
+            'under_odds': odds_data.get('underOdds')
+        }
+    
+    def _parse_last_updated(self, odds_data):
+        """Parse last updated timestamp"""
+        if not odds_data or not isinstance(odds_data, dict):
+            return None
+        return odds_data.get('lastUpdated')
+    
+    @classmethod
+    async def _get_async(cls, game_id):
+        """
+        Get odds data by game ID
+        
+        Args:
+            game_id (int): The ESPN game ID
+            
+        Returns:
+            Odds: An Odds object with the odds data
+        """
+        if not isinstance(game_id, int):
+            raise TypeError("game_id must be an integer")
+            
+        try:
+            # Get odds data
+            odds_url = f"http://sports.core.api.espn.com/v2/sports/football/leagues/nfl/events/{game_id}/competitions/{game_id}/odds"
+            odds_data = await get_request(odds_url)
+            
+            # Create and return Odds object
+            return cls(odds_data)
+            
+        except Exception as e:
+            raise Exception(f"Error fetching odds data: {str(e)}")
+    
+    def get_favorite(self):
+        """Get the favorite team based on the odds"""
+        if self.moneyline['home']['favorite']:
+            return 'home'
+        elif self.moneyline['away']['favorite']:
+            return 'away'
+        return None
+    
+    def get_underdog(self):
+        """Get the underdog team based on the odds"""
+        if self.moneyline['home']['favorite'] is False:
+            return 'home'
+        elif self.moneyline['away']['favorite'] is False:
+            return 'away'
+        return None
+    
+    def get_implied_probability(self, odds):
+        """
+        Convert American odds to implied probability
+        
+        Args:
+            odds (int): American odds (positive or negative)
+            
+        Returns:
+            float: Implied probability (0-1)
+        """
+        if not odds:
+            return None
+            
+        if odds > 0:
+            return 100 / (odds + 100)
+        else:
+            return abs(odds) / (abs(odds) + 100)
+    
+    def __str__(self):
+        if not self.raw_data:
+            return "No odds data available"
+            
+        favorite = self.get_favorite()
+        underdog = self.get_underdog()
+        
+        if not favorite or not underdog:
+            return "No clear favorite/underdog"
+            
+        favorite_odds = self.moneyline[favorite]['odds']
+        underdog_odds = self.moneyline[underdog]['odds']
+        
+        spread = self.spread[underdog]['point_spread']
+        over_under = self.over_under.get('line', 'N/A')
+        
+        return (
+            f"Spread: {underdog.capitalize()} {spread} ({underdog_odds if underdog_odds > 0 else ''})\n"
+            f"Moneyline: {favorite.capitalize()} {favorite_odds if favorite_odds > 0 else ''} | "
+            f"{underdog.capitalize()} {underdog_odds if underdog_odds > 0 else ''}\n"
+            f"Total: {over_under} (O/U)"
+        )
